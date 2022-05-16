4320dbba18c6ef75328b143642c587a770c044e6
Hristo1500 <72629943+Hristo1500@users.noreply.github.com>
2022-05-16 20:27:13 +0300

    Add files via upload

---
+++ MinimalMVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MinimalMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MinimalMVVM.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Presenter presenterUpper = new Presenter();
+        private PresenterLower presenterLower = new PresenterLower();
+        public MainWindow()
+        {
+            DataContext = presenterUpper;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(LowerButton.IsChecked==true)
+            {
+
+                DataContext = this.presenterLower;
+            }
+            else
+            {
+                DataContext=this.presenterUpper;
+            }
+        }
+    }
+}
